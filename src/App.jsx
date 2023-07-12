17ccc3bb8d8cc038b575f8e2e8953308c80be38c
Jan Poonthong <janpoonthong628@gmail.com>
2023-07-12 20:44:52 +0700

    feat: implement adding item

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,46 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [text, setText] = useState("")
+  const [tasks, setTasks] = useState([]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const formJson = Object.fromEntries(formData.entries());
+    setTasks([...tasks, formJson.userInput]);
+  }
 
   return (
     <>
-        <div className="container">
-            <div className="user-input">
-                <button className="add-button">+</button>
-                <input className="" type="text"></input>
-            </div>
-
-            <div>
-                <div className="each-todo">
-                    <p className="task">Hello</p>
-                    <button>X</button>
-                </div>
-            </div>
+      <h1>Todo Website</h1>
+      <div className="container">
+        <div className="user-input">
+          <form onSubmit={handleSubmit}>
+            <button className="add-button" type="submit">
+              +
+            </button>
+            <label>
+              <input className="input-field" name="userInput" type="text" />
+            </label>
+          </form>
         </div>
+
+        <div>
+          <div className="each-todo">
+            {tasks.map((task, index) => (
+              <div key={index}>
+                <input type="checkbox" />
+                <p className="task">{task}</p>
+                <button>X</button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
