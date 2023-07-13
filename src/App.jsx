09da460174b30a577215a418f22da7fce0e59acf
Jan Poonthong <janpoonthong628@gmail.com>
2023-07-13 07:02:00 +0700

    build: production

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,27 @@ import "./App.css";
 function App() {
   const [tasks, setTasks] = useState([]);
 
+  function handleRemove(index, clickedTask) {
+    // FIXME(jan): same word will get remove
+    const newTasks = tasks.filter((task) => task !== clickedTask);
+    console.log(tasks.filter((task) => task !== clickedTask));
+    setTasks(newTasks);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
+
     const formData = new FormData(event.target);
     const formJson = Object.fromEntries(formData.entries());
-    if (formJson.userInput === "") {
-      alert("You need a task to add");
-    } else {
-      setTasks([...tasks, formJson.userInput]);
-      event.target.reset();
+
+    setTasks([...tasks, formJson.userInput]);
+    event.target.reset();
+  }
+
+  function handleOnChange(event) {
+    let userInputLength = event.target.value.length;
+    if (userInputLength === 250) {
+      alert("Must be less than 250 characters");
     }
   }
 
@@ -31,22 +43,29 @@ function App() {
                 className="input-field"
                 name="userInput"
                 type="text"
-                placeholder="add task"
+                placeholder="Add task"
+                maxLength="250"
+                onChange={handleOnChange}
+                required
               />
             </label>
           </form>
         </div>
 
-        <div>
-          <div className="each-todo">
-            {tasks.map((task, index) => (
-              <div key={index}>
-                <input type="checkbox" />
-                <p className="task">{task}</p>
-                <button>X</button>
-              </div>
-            ))}
-          </div>
+        <div className="each-todo">
+          {tasks.map((task, index) => (
+            <div key={index}>
+              <input type="checkbox" />
+              <p className="task">{task}</p>
+
+              <button
+                onClick={() => handleRemove(index, task)}
+                className="remove-button"
+              >
+                X
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </>
