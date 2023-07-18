aaf8b52bdcbbd496bb6a306fd30f25a4b74c19fe
Jan Poonthong <janpoonthong628@gmail.com>
2023-07-18 12:13:57 +0700

    refactor: form, function, and variables

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import { v4 as uuidv4 } from "uuid";
 
+const WORD_LIMIT = 100;
+const ERROR_EMPTY_TASK = "Please enter a task";
+
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [countCompleted, setCountCompleted] = useState(0);
 
   useEffect(() => {
     const data = localStorage.getItem("data");
@@ -19,45 +23,59 @@ function App() {
   function handleRemove(task) {
     const updatedTasks = tasks.filter((t) => t.id !== task.id);
     setTasks(updatedTasks);
+
+    setCountCompleted(getCompletedTaskCount(updatedTasks));
   }
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    const formData = new FormData(event.target);
-    const formJson = Object.fromEntries(formData.entries());
+    const form = event.target;
+    const userInput = form.userInput.value.trim();
+    if (userInput.length === 0) {
+      return alert(ERROR_EMPTY_TASK);
+    }
+
     const newTask = {
       id: uuidv4(),
-      text: formJson.userInput,
+      text: userInput,
       completed: false,
     };
 
-    setTasks([...tasks, newTask]);
-    event.target.reset();
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+    form.reset();
   }
 
-  function handleOnChange(event) {
-    let userInputLength = event.target.value.length;
-    const wordLimit = 100;
-    if (userInputLength === wordLimit) {
-      alert(`Must be less than ${wordLimit} characters`);
+  function handleOnChange({ target }) {
+    let userInputLength = target.value.length;
+    if (userInputLength === WORD_LIMIT) {
+      alert(`Must be less than ${WORD_LIMIT} characters`);
     }
   }
 
-  function handleCheckBox(event, task) {
+  function handleCheckBox({ target }, task) {
     const updatedTasks = [...tasks];
     const taskIndex = tasks.findIndex((item) => item.id === task.id);
-    if (event.target.checked) {
-      updatedTasks[taskIndex].completed = true;
-    } else {
-      updatedTasks[taskIndex].completed = false;
-    }
+    updatedTasks[taskIndex].completed = target.checked;
     setTasks(updatedTasks);
+    setCountCompleted(getCompletedTaskCount(updatedTasks));
+  }
+
+  function getCompletedTaskCount(taskList) {
+    return taskList.reduce(
+      (count, task) => (task.completed ? count + 1 : count),
+      0,
+    );
   }
 
   return (
     <>
       <h1>Todo Website</h1>
+      <div className="completed-count">
+        <p>
+          Completed: {countCompleted}/{tasks.length}
+        </p>
+      </div>
       <div className="container">
         <div className="user-input">
           <form onSubmit={handleSubmit}>
@@ -70,7 +88,7 @@ function App() {
                 name="userInput"
                 type="text"
                 placeholder="Add task"
-                maxLength="100"
+                maxLength={WORD_LIMIT}
                 onChange={handleOnChange}
                 required
               />
